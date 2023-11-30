ba1855812e956dc18d78341f3c8f21407831bb57
Israel Alvarez <brody.3@gmail.com>
2023-11-30 12:52:11 -0600

    method for test

--- src/LaravelFacturaMx.php
+++ src/LaravelFacturaMx.php
@@ -1,7 +1,11 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace JiagBrody\LaravelFacturaMx;
 
 class LaravelFacturaMx
 {
+    public function testing(): bool
+    {
+        return false;
+    }
 }
