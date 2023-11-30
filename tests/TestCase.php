06c946211b32d76e56068e52ae9f5f3fb523706a
jiagbrody <jiagbrody@users.noreply.github.com>
2023-11-30 17:52:21 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace JiagBrody\LaravelFacturaMx\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use JiagBrody\LaravelFacturaMx\LaravelFacturaMxServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
